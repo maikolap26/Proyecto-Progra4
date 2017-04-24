9712fcbeb5134ecd224b3187c5ff8772d406db25
maikolap26 <maikolap26@gmail.com>
2017-04-23 21:32:29 -0600

    Avance
    Se registra usuario

--- Proyecto/js/Metodos.js
+++ Proyecto/js/Metodos.js
@@ -5,13 +5,14 @@ function pageLoad() {
     var btn1=document.getElementById("signup");
     var campoUs = document.getElementById("user");
     var campoPass = document.getElementById("pass");
+    var formulario= document.getElementById("formulario");
     btn.addEventListener("click", inicioSesion);
     btn1.addEventListener("click",redireccionar);
     campoUs.addEventListener("focus", doFocus);
     campoUs.addEventListener("blur", doBlur);
     pass.addEventListener("focus", doFocus);
     pass.addEventListener("blur", doBlur);
-    
+    formulario.addEventListener("submit",doValidate);
 }
 
 function doFocus(event) {
@@ -35,14 +36,121 @@ function inicioSesion() {
         var sp=document.getElementById("span");
         var p = document.createElement("p");
         var a= document.createElement("a");
+		var ul=document.createElement("a");
+		var li=document.createElement("li");
+		li.appendChild(document.createTextNode("Cerrar Sesión"));
+		li.className="cerrar";
         a.className="anchor";
         a.appendChild(document.createTextNode(usuario.usuario));
+		a.appendChild(li);
         p.appendChild(a);
-        sp.appendChild(p);
+		ul.appendChild(p);
+        sp.appendChild(ul);
         
     }
 
 }
 
+function doValidate(){
+    var user=document.getElementById("usuario");
+    var contraseña=document.getElementById("contraseña");
+    var cedula=document.getElementById("cedula");
+    var nombre=document.getElementById("nombre");
+    var apellido=document.getElementById("apellidos");
+    var correo=document.getElementById("correo");
+    var telefono=document.getElementById("telefono");
+    var celular=document.getElementById("celular");
+    var fecha=document.getElementById("fechaNacimiento");
+    var error=false;
+    
+    if(user != null){
+		if(user.value == ""){
+			error=true;
+			user.className="error";
+		}
+	}	
+    
+    if(contraseña != null){
+		if(contraseña.value == ""){
+			error=true;
+			contraseña.className="error";
+		}
+	}
+    
+    if(cedula != null){
+		if(cedula.value == ""){
+			error=true;
+			cedula.className="error";
+		}
+	}
+    
+    if(nombre != null){
+		if(nombre.value == ""){
+			error=true;
+			nombre.className="error";
+		}
+	}
+    
+    if(apellido != null){
+		if(apellido.value == ""){
+			error=true;
+			apellido.className="error";
+		}
+	}
+    
+    if(correo != null){
+		if(correo.value == ""){
+			error=true;
+			correo.className="error";
+		}
+	}
+    
+    if(telefono != null){
+		if(telefono.value == ""){
+			error=true;
+			telefono.className="error";
+		}
+	}
+    
+    if(celular != null){
+		if(celular.value == ""){
+			error=true;
+			celular.className="error";
+		}
+	}
+    
+    if(fecha != null){
+		if(fecha.value == ""){
+			error=true;
+			fecha.className="error";
+		}
+	}
+    
+    if(error == true){
+		event.preventDefault();
+	}
+    
+}
+
+function doSubmit(){
+	var user=document.getElementById("usuario");
+    var contraseña=document.getElementById("contraseña");
+    var cedula=document.getElementById("cedula");
+    var nombre=document.getElementById("nombre");
+    var apellido=document.getElementById("apellidos");
+    var correo=document.getElementById("correo");
+    var telefono=document.getElementById("telefono");
+    var celular=document.getElementById("celular");
+    var fecha=document.getElementById("fechaNacimiento");
+	
+	usuario= new Usuario(cedula.value,user.value,contraseña.value,nombre.value,apellido.value,correo.value,telefono.value,celular.value,fecha.value);
+	
+	var formulario= document.getElementById("formulario");
+	usuarios.push(usuario);
+	StorageUsuario.store("usuarios",usuarios);
+	window.alert("Data sent: "+ usuario.usuario);
+	formulario.reset();
+	
+}
 
 document.addEventListener("DOMContentLoaded", pageLoad);
