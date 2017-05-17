2d4e0f1971a7f8605e17872bbb44f6b93b12eeb0
maikolap26 <maikolap26@gmail.com>
2017-05-17 10:55:29 -0600

    Cambios modelo-Login
    Login

--- Progra4_project/src/java/AAMAirline/service/AAMAirlinesService.java
+++ Progra4_project/src/java/AAMAirline/service/AAMAirlinesService.java
@@ -1,11 +1,12 @@
-
 package AAMAirline.service;
 
 import AAMAirline.model.AAMAirlineModel;
 import AAMAirline.model.Avion;
 import AAMAirline.model.Ciudad;
 import AAMAirline.model.Jsonable;
+import AAMAirline.model.Login;
 import AAMAirline.model.Ruta;
+import AAMAirline.model.Usuario;
 import AAMAirline.model.Vuelo;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -18,55 +19,74 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-                  
-@WebServlet(name="AAMAirlinesService", urlPatterns= {"/AAMAirlinesService"})
-public class AAMAirlinesService extends HttpServlet {
-    AAMAirlineModel model;
-	protected void processRequest(HttpServletRequest request,HttpServletResponse response)
-		throws ServletException, IOException{
-			response.setContentType("text/html;charset=UTF-8");
-			try(PrintWriter out=response.getWriter()){
-				response.setContentType("text/xml");
-				RuntimeTypeAdapterFactory<Jsonable> rta = RuntimeTypeAdapterFactory.of(Jsonable.class,"_class")
-				.registerSubtype(Ciudad.class,"Ciudad")
-                                .registerSubtype(Ruta.class,"Ruta")
-                                .registerSubtype(Avion.class,"Avion")
-				.registerSubtype(Vuelo.class,"Vuelo");
-				Gson gson=new GsonBuilder().registerTypeAdapterFactory(rta).setDateFormat("dd/MM/yyyy").create();
-				String json;
-				String accion=request.getParameter("action");
-				System.out.println(accion);
-				List<Ciudad> ciudades;
-				List<Vuelo> vuelos;
-				switch(accion){
-					case "ciudadListAll":
-						ciudades=model.getCiudades1();
-						json=gson.toJson(ciudades);
-						out.write(json);
-						break;
-					case "vueloListPromo":
-						vuelos=model.getVuelos();
-						json=gson.toJson(vuelos);
-						out.write(json);
-						break;
-					case "vueloListSearch":
-						String origen=request.getParameter("origen");
-						String destino=request.getParameter("destino");
-                                                vuelos=model.getVuelos(origen,destino);
-						json=gson.toJson(vuelos);
-						out.write(json);
-						break;
-				}
-			}
-			catch(Exception e){System.out.println(e);}
-		}
-		
-		@Override
-		public void init() throws ServletException{
-			super.init();
-			model=new AAMAirlineModel();
-		}
 
+@WebServlet(name = "AAMAirlinesService", urlPatterns = {"/AAMAirlinesService"})
+public class AAMAirlinesService extends HttpServlet {
+
+    AAMAirlineModel model;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            response.setContentType("text/xml");
+            RuntimeTypeAdapterFactory<Jsonable> rta = RuntimeTypeAdapterFactory.of(Jsonable.class, "_class")
+                    .registerSubtype(Ciudad.class, "Ciudad")
+                    .registerSubtype(Ruta.class, "Ruta")
+                    .registerSubtype(Avion.class, "Avion")
+                    .registerSubtype(Vuelo.class, "Vuelo");
+            Gson gson = new GsonBuilder().registerTypeAdapterFactory(rta).setDateFormat("dd/MM/yyyy").create();
+            String json;
+            String accion = request.getParameter("action");
+            System.out.println(accion);
+            List<Ciudad> ciudades;
+            List<Vuelo> vuelos;
+            switch (accion) {
+                case "ciudadListAll":
+                    ciudades = model.getCiudades1();
+                    json = gson.toJson(ciudades);
+                    out.write(json);
+                    break;
+                case "vueloListPromo":
+                    vuelos = model.getVuelos();
+                    json = gson.toJson(vuelos);
+                    out.write(json);
+                    break;
+                case "vueloListSearch":
+                    String origen = request.getParameter("origen");
+                    String destino = request.getParameter("destino");
+                    vuelos = model.getVuelos(origen, destino);
+                    json = gson.toJson(vuelos);
+                    out.write(json);
+                    break;
+                case "userLogin":
+                    Login us = new Login(request.getParameter("user"), request.getParameter("pass"), "0");
+                    us = model.userLogin(us);
+                    if (us.getTipo() != "0") {
+                        request.getSession().setAttribute("user", us);
+                        switch (us.getTipo()) {
+                            case "1": // client
+                                Usuario client = model.clientGet(us.getUsuario());
+                                request.getSession().setAttribute("client", client);
+                                request.getRequestDispatcher("/PaginaPrincipal.html").forward(request, response);
+                                break;
+                            case "2": // manager
+                                request.getRequestDispatcher("/ManagerMenu.jsp").forward(request, response);
+                                break;
+                        }
+                    }
+
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        model = new AAMAirlineModel();
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
